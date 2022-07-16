402aca7ec8b102aa7850762999095511ed96bf39
ngugimuchangi <tech.curious14@gmail.com>
2022-07-16 13:03:49 +0000

    check for wrong remote origin

--- github_scripts/init.sh
+++ github_scripts/init.sh
@@ -10,22 +10,16 @@ if [ "$(echo $?)" = "0" ]; then
 			git branch -M master
 			if [ "$(echo $?)" = "0" ]; then
 				read -p "$(tput setaf 2)Enter link to your remote:$(tput setaf 7) " remote
-				git remote add origin $remote
+				git remote add origin "$remote"
 				if [ "$(echo $?)" = "0" ]; then
 					git push
 					if [ "$(echo $?)" = "0" ]; then
 						echo "$(tput setaf 2)Successful initialization"
 					else
-						echo "Failed to initalize. Check github error for more details" 1>&2
-						git remove remote origin $remote
-						git restore --mixed HEAD~
+						echo "Failed to initalize. Try again" 1>&2
+						rm -rf .git
 						exit 1
 					fi
-				else
-					git remove remote origin $remote
-					git restore --mixed HEAD~
-					echo "Remote add failed try again" 1>&2
-					exit 1
 				fi
 			fi
 		fi
