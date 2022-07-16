853fff11704045bf90a1dc2b3f0d1d288374728c
ngugimuchangi <tech.curious14@gmail.com>
2022-07-16 15:47:50 +0000

    installation final fix

--- github_scripts/installation.sh
+++ github_scripts/installation.sh
@@ -1,10 +1,10 @@
-#!/bin/bashi -x
-if [-d "$HOME/.local/bin"] && [ !-d "$HOME/bin" ]; then
-	cp  *.sh $HOME/.local/bin/
+#!/bin/bash
+if [-d "$HOME/.local/bin"] && [ ! -d "$HOME/bin" ]; then
+	cp *.sh $HOME/.local/bin/
 	rm $HOME/.local/bin/installation.sh
 	echo "$(tput setaf 2)Sucessful installation"
 elif [ -d "$HOME/bin" ]; then
-	cp  *.sh $HOME/bin/
+	cp *.sh $HOME/bin/
 	rm $HOME/bin/installation.sh
 	echo "$(tput setaf 2)Sucessful installation"
 else
