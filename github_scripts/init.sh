f58e0d9f36fd18b13f64a1b9b1baabfac190751e
ngugimuchangi <tech.curious14@gmail.com>
2022-07-16 13:12:02 +0000

    create upstream branch for new repo

--- github_scripts/init.sh
+++ github_scripts/init.sh
@@ -12,11 +12,11 @@ if [ "$(echo $?)" = "0" ]; then
 				read -p "$(tput setaf 2)Enter link to your remote:$(tput setaf 7) " remote
 				git remote add origin "$remote"
 				if [ "$(echo $?)" = "0" ]; then
-					git push
+					git push -u origin master
 					if [ "$(echo $?)" = "0" ]; then
 						echo "$(tput setaf 2)Successful initialization"
 					else
-						echo "Failed to initalize. Try again" 1>&2
+						echo "Failed to initalize. Try again" 1>2
 						rm -rf .git
 						exit 1
 					fi
